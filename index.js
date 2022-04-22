3406758fc8c8d9c90f86c1d0de4987411ec14eed
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2022-04-22 16:29:09 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+// The code below is temporary and can be commented out.
+const { fetchMyIP, fetchCoordsByIP } = require('./iss');
+
+fetchMyIP((error, ip) => {
+  if (error) {
+    console.log("It didn't work!", error);
+    return;
+  }
+
+  console.log('It worked! Returned IP:', ip);
+});
+
+fetchCoordsByIP('162.58.222.72', (error, coordinates) => {
+  if (error) {
+    console.log("It didn't work!", error);
+    return;
+  }
+  console.log('It worked! Returned coordinates:', coordinates);
+});
