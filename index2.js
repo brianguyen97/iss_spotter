811ff5d7c5659f8d0c09db2966ec7f6cb43c18ee
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2022-04-24 17:55:13 -0600

    Update index2.js

--- index2.js
+++ index2.js
@@ -1,10 +1,9 @@
-const {
-  fetchMyIP,
-  fetchCoordsByIP,
-  fetchISSFlyOverTimes,
-} = require('./iss_promised');
+const { nextISSTimesForMyLocation } = require('./iss_promised');
 
-fetchMyIP()
-  .then(fetchCoordsByIP)
-  .then(fetchISSFlyOverTimes)
-  .then(body => console.log(body));
+nextISSTimesForMyLocation()
+  .then(passTimes => {
+    printPassTimes(passTimes);
+  })
+  .catch(error => {
+    console.log("It didn't work: ", error.message);
+  });
