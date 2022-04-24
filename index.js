b2c116c3c9d08dd5c81c5b59fe3da1afe8f7db5e
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2022-04-24 17:55:10 -0600

    Update index.js

--- index.js
+++ index.js
@@ -34,12 +34,27 @@ fetchISSFlyOverTimes(exampleCoords, (error, passTimes) => {
   console.log('It worked! Returned flyover times:', passTimes);
 });
 
-const { nextISSTimesForMyLocation } = require('./iss');
-
 nextISSTimesForMyLocation((error, passTimes) => {
   if (error) {
     return console.log("It didn't work!", error);
   }
   // success, print out the deets!
   console.log(passTimes);
+});
+
+const printPassTimes = function (passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  // success, print out the deets!
+  printPassTimes(passTimes);
 });
