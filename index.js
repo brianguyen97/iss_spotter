2a3ffb903ffcf60933e5154c0435529b9d0f31a0
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2022-04-24 17:41:53 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 // The code below is temporary and can be commented out.
-const { fetchMyIP, fetchCoordsByIP } = require('./iss');
+const {
+  fetchMyIP,
+  fetchCoordsByIP,
+  fetchISSFlyOverTimes,
+  nextISSTimesForMyLocation,
+} = require('./iss');
 
 fetchMyIP((error, ip) => {
   if (error) {
@@ -10,10 +15,31 @@ fetchMyIP((error, ip) => {
   console.log('It worked! Returned IP:', ip);
 });
 
-fetchCoordsByIP('162.58.222.72', (error, coordinates) => {
+fetchCoordsByIP('50.93.99.128', (error, coordinates) => {
   if (error) {
     console.log("It didn't work!", error);
     return;
   }
   console.log('It worked! Returned coordinates:', coordinates);
+});
+
+const exampleCoords = { latitude: '49.27670', longitude: '-123.13000' };
+
+fetchISSFlyOverTimes(exampleCoords, (error, passTimes) => {
+  if (error) {
+    console.log("It didn't work!", error);
+    return;
+  }
+
+  console.log('It worked! Returned flyover times:', passTimes);
+});
+
+const { nextISSTimesForMyLocation } = require('./iss');
+
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  // success, print out the deets!
+  console.log(passTimes);
 });
